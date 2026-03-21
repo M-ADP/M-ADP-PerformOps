d3d017c9ea0bc67bdec277dd34c51c918186d3d0
박동현 <pdh0128a@gmail.com>
2026-03-21 17:54:53 +0900

    feat(PerformOps): Sentry 에러 로그 조히

--- src/common/config.py
+++ src/common/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     database_url: str
     prometheus_url: str
     resource_manager_url: str
+    sentry_auth_token: str
+    sentry_organization_slug: str
 
     class Config:
         env_file = ".env"
--- src/infra/client/sentry_error_tracker.py
+++ src/infra/client/sentry_error_tracker.py
@@ -1,7 +1,26 @@
+from src.common.config import settings
 from src.core.error_tracker import ErrorTracker
+from src.core.requester import Requester
 
 
-class SentryErrorTacker(ErrorTracker):
+class SentryErrorTracker(ErrorTracker):
 
-    async def get_error(self, project_id: int, app_deployment_name: str):
-        pass
+    def __init__(self, requester: Requester):
+        self._requester = requester
+        self._base_url = "https://sentry.io/api/0"
+        self._headers = {"Authorization": f"Bearer {settings.sentry_auth_token}"}
+
+    async def get_error(
+            self,
+            project_id: int,
+            app_deployment_name: str,
+    ):
+        return await self._requester.get(
+            url=(
+                f"{self._base_url}/projects"
+                f"/{settings.sentry_organization_slug}"
+                f"/{project_id}/issues/"
+            ),
+            params={"query": f"project_id:{project_id} app_deployment_name:{app_deployment_name}"},
+            headers=self._headers,
+        )
