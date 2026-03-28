08599e40f217267e4e37f36aa285fb217d035463
박동현 <pdh0128a@gmail.com>
2026-03-29 01:20:17 +0900

    refactor(PerformOps API): 응답에 actions 포함

--- src/api/performops.py
+++ src/api/performops.py
@@ -3,26 +3,31 @@ from fastapi import APIRouter, Depends
 from src.app.get_list import GetPerformopsListUseCase
 from src.app.start_performops import StartPerformopsListUseCase
 from src.common.schema import CursorPage, CursorRequest, SuccessResponse
-from src.core.performops.model import PerformOpsResult
+from src.core.performops.model import Performops, PerformOpsResult
 
-performops_router = APIRouter(prefix='/performops', tags=['performops'])
+performops_router = APIRouter(prefix="/performops", tags=["performops"])
 
 
-@performops_router.get('/{project_id}', response_model=SuccessResponse[CursorPage])
+@performops_router.get(
+    "/{project_id}", response_model=SuccessResponse[CursorPage[Performops]]
+)
 async def get_performops(
-        project_id: int,
-        cursor_request: CursorRequest = Depends(),
-        usecase: GetPerformopsListUseCase = Depends(GetPerformopsListUseCase),
+    project_id: int,
+    cursor_request: CursorRequest = Depends(),
+    usecase: GetPerformopsListUseCase = Depends(GetPerformopsListUseCase),
 ):
     data = await usecase(project_id=project_id, cursor_request=cursor_request)
     return SuccessResponse(message="ok", data=data)
 
 
-@performops_router.post('/{project_id}/{app_deployment_name}', response_model=SuccessResponse[PerformOpsResult])
+@performops_router.post(
+    "/{project_id}/{app_deployment_name}",
+    response_model=SuccessResponse[PerformOpsResult],
+)
 async def start_performops(
-        project_id: int,
-        app_deployment_name: str,
-        usecase: StartPerformopsListUseCase = Depends(StartPerformopsListUseCase),
+    project_id: int,
+    app_deployment_name: str,
+    usecase: StartPerformopsListUseCase = Depends(StartPerformopsListUseCase),
 ):
     data = await usecase(project_id=project_id, app_deployment_name=app_deployment_name)
     return SuccessResponse(message="ok", data=data)
